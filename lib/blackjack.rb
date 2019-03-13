c32cf835759c45d33291be1f5f500be15ca9a544
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-13 16:41:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,16 +1,13 @@
 def welcome
   # code #welcome here
-  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
- card = 1 + rand(11)
 end
 
 def display_card_total(card_total)
   # code #display_card_total here
-  puts "Your cards add up to #{card_total}."
 end
 
 def prompt_user
@@ -25,7 +22,7 @@ end
 
 def end_game(card_total)
   # code #end_game here
-  "Sorry, you hit #{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
