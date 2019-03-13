0b32375a7439def940f0495f36baf7213c614608
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-13 16:29:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,7 +5,7 @@ end
 
 def deal_card
   # code #deal_card here
-  card_value = 1 + rand(11)
+  1 + rand(11)
 end
 
 def display_card_total(card_total)
@@ -15,7 +15,7 @@ end
 
 def prompt_user
   # code #prompt_user here
-  "Type 'h' to hit or 's' to stay."
+  puts "Type 'h' to hit or 's' to stay."
 end
 
 def get_user_input
